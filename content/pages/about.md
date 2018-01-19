644bdb12bf61e18d3d3b082b2405cf3eb906861b
Nicolas Silva <nical@fastmail.com>
2018-01-19 01:01:10 +0100

    Initial blog setup.

---
+++ content/pages/about.md
@@ -0,0 +1,14 @@
+Title: About me
+Date: 2017-10-29
+
+# Nicolas Silva
+
+(But people tend to call me Nical)
+
+## Work
+
+Gfx software engineer at [Mozilla](https://www.mozilla.org/en-US/).
+
+## Blog
+
+This blog was built using [Pelican](https://blog.getpelican.com/) and a modified version of the [svbhack](https://github.com/gfidente/pelican-svbhack/tree/3170593077434e4d12604c6124ac97008ad41f04) theme.
---
+++ pelicanconf.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Nical'
+AUTHOR_NAME='Nicolas Silva'
+SITENAME = u'Eight million pixels and counting'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+THEME='theme/svbhack'
+ARTICLE_PATHS = ['posts', 'images']
+PAGE_PATHS = ['pages', 'images']
+STATIC_PATHS = ['images']
+
+# aside
+LINKS = ()
+SOCIAL = (
+    ('mastodon', 'https://mastodon.gamedev.place/@Nical'),
+    ('twitter', 'https://twitter.com/nical'),
+    ('github', 'https://github.com/nical'),
+    ('mozgfx', 'http://mozillagfx.wordpress.com/'),
+)
+
+DISPLAY_PAGES_ON_MENU = False
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
