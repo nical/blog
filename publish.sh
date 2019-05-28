942806d990caca55b3b60b25268735a110737b8a
Nicolas Silva <nical@fastmail.com>
2019-05-28 23:44:34 +0200

    Add a publish.sh script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo "building..." &&
+pelican ./content &&
+echo "copying into nical.github.io..." &&
+cp -r ./output/* ../nical.github.io/ &&
+echo "git add . && git commit -am $1" &&
+cd ../nical.github.io/ &&
+git add . &&
+git commit -am $1 &&
+echo "Blog updated, push nical.github.io to publish the modifications online."
